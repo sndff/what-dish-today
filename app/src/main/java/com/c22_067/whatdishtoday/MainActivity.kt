095aa1c8aaac6214f9a6400bdcccda6b0484345d
Alippp1 <aliftafn03@upi.edu>
2022-12-05 22:26:20 +0700

    login done, but masih ada bug dan form loginnya perlu diperbagus

--- app/src/main/java/com/c22_067/whatdishtoday/MainActivity.kt
+++ app/src/main/java/com/c22_067/whatdishtoday/MainActivity.kt
@@ -1,41 +1,121 @@
 package com.c22_067.whatdishtoday
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import com.c22_067.whatdishtoday.databinding.ActivityMainBinding
+import com.c22_067.whatdishtoday.databinding.PopupLoginBinding
 import com.c22_067.whatdishtoday.ui.home.HomeActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnLogin.setOnClickListener {
-            val dialogBuilder = AlertDialog.Builder(this)
-            val popUp = layoutInflater.inflate(R.layout.popup_login, null)
-            dialogBuilder.setTitle("Sign In")
-            dialogBuilder.setView(popUp)
-            dialogBuilder.show()
-        }
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
-        binding.btnRegister.setOnClickListener {
-            val dialogBuilder = AlertDialog.Builder(this)
-            val popUp = layoutInflater.inflate(R.layout.popup_register, null)
-            dialogBuilder.setTitle("Sign Up")
-            dialogBuilder.setView(popUp)
-            dialogBuilder.show()
-        }
-
-        binding.btnHome.setOnClickListener {
-            val i = Intent(this, HomeActivity::class.java)
-            startActivity(i)
+        binding.btnGoogle.setOnClickListener {
+            signIn()
         }
     }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        resultLauncher.launch(signInIntent)
+    }
+
+    private var resultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "Login Berhasil")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Log.w(TAG, "Login Gagal", task.exception)
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if (currentUser != null) {
+            startActivity(Intent(this@MainActivity, HomeActivity::class.java))
+            finish()
+        }
+    }
+
+    companion object {
+        private const val TAG = "LoginActivity"
+    }
 }
+
+
+
+//        binding.btnLogin.setOnClickListener {
+//            val dialogBuilder = AlertDialog.Builder(this)
+//            val popUp = layoutInflater.inflate(R.layout.popup_login, null)
+//            dialogBuilder.setTitle("Sign In")
+//            dialogBuilder.setView(popUp)
+//            dialogBuilder.show()
+//        }
+//
+//        binding.btnRegister.setOnClickListener {
+//            val dialogBuilder = AlertDialog.Builder(this)
+//            val popUp = layoutInflater.inflate(R.layout.popup_register, null)
+//            dialogBuilder.setTitle("Sign Up")
+//            dialogBuilder.setView(popUp)
+//            dialogBuilder.show()
+//        }
+//
+//        binding.btnHome.setOnClickListener {
+//            val i = Intent(this, HomeActivity::class.java)
+//            startActivity(i)
+//        }
+//    }
+//}
