7e7d6243ff3a6bb646e6d7e68ff72ea9f3ddfa10
hanips <hanippw@students.unnes.ac.id>
2022-12-09 08:34:25 +0700

    Splash Screen

--- app/src/main/java/com/c22_067/whatdishtoday/MainActivity.kt
+++ app/src/main/java/com/c22_067/whatdishtoday/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.c22_067.whatdishtoday.databinding.ActivityMainBinding
 import com.c22_067.whatdishtoday.ui.home.HomeActivity
 
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
